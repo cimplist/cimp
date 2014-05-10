cfc07cbcb3a4f78bd969834a6ec4e72b247afc2a
sanjay <sanjays30@yahoo.com>
2014-05-11 01:53:05 +0530

    added REST test

---
+++ cip/src/main/java/com/cimplist/cip/user/web/rest/UserProfileRESTroller.java
@@ -0,0 +1,33 @@
+package com.cimplist.cip.user.web.rest;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cimplist.cip.user.domain.User;
+import com.cimplist.cip.user.svc.UserProfileService;
+
+@RestController
+@RequestMapping(value = "/rest/user")	
+public class UserProfileRESTroller {
+	private static final Logger logger = LoggerFactory.getLogger(UserProfileRESTroller.class);
+	@Inject
+	private UserProfileService userService;
+
+	@RequestMapping(value = "/{userName}" , method = RequestMethod.GET, produces="application/json")
+	@ResponseStatus(HttpStatus.OK)
+	public User get(@PathVariable("userName") String  userName) {  
+		logger.info("Calling JSON service /user/"+userName);
+		User user = userService.getUserWithTeamByUserName(userName);
+		return user;
+	}
+
+	
+}
---
+++ cip/src/test/java/com/cimplist/cip/user/web/rest/UserProfileRESTrollerMockTest.java
@@ -0,0 +1,58 @@
+package com.cimplist.cip.user.web.rest;
+import static org.junit.Assert.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.cimplist.cip.user.web.rest.UserProfileRESTroller;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(locations={
+		"classpath:spring/mvc-config-test.xml",		
+		"classpath:spring/application-config.xml"
+		})
+public class UserProfileRESTrollerMockTest {
+private static final String BASE_URI = "/rest";
+    private MockMvc mockMvc;
+    
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    
+    @Autowired
+    private UserProfileRESTroller userProfileRestController;
+    
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+    @Test
+    public void testGetUser(){
+    	try {
+			ResultActions result  = mockMvc.perform(get(BASE_URI+"/user/{userName}", "samm"));
+			result.andDo(print());
+			result.andExpect(status().isOk());
+			result.andExpect(content().contentType(MediaType.APPLICATION_JSON));
+			result.andExpect(jsonPath("key").value(3));
+			result.andExpect(jsonPath("userName").value("Sam"));
+		} catch (Exception e) {
+			e.printStackTrace();
+			assertTrue(e.toString(),false);
+		}
+    }
+}
