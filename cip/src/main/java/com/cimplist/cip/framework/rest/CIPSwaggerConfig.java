ce46507efa26c6b4ed9737ae7b68002bd1518b53
sanjay <sanjays30@yahoo.com>
2014-05-27 22:23:30 +0530

    added security for swagger-ui

--- cip/src/main/java/com/cimplist/cip/framework/rest/CIPSwaggerConfig.java
+++ cip/src/main/java/com/cimplist/cip/framework/rest/CIPSwaggerConfig.java
@@ -1,8 +1,25 @@
 package com.cimplist.cip.framework.rest;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.wordnik.swagger.model.ApiInfo;
 
 @Configuration
 public class CIPSwaggerConfig {
-
+	/**
+	   * API Info as it appears on the swagger-ui page
+	   */
+	@Bean
+	  public ApiInfo apiInfo() {
+	    ApiInfo apiInfo = new ApiInfo(
+	        "Demo Spring MVC swagger 1.2 api",
+	        "CIP spring mvc api based on the swagger 1.2 spec",
+	        "http://en.wikipedia.org/wiki/Terms_of_service",
+	        "sanjays30@yahoo.com",
+	        "Apache 2.0",
+	        "http://www.apache.org/licenses/LICENSE-2.0.html"
+	    );
+	    return apiInfo;
+	  }
 }
